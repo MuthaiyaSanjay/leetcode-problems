2d6a912fcd162dd41e1567abfbd8ae69c03def92
muthaiya <muthaiya3152@gmail.com>
2025-01-14 17:10:42 +0530

    star pattern using java in simple logic

---
+++ src/basic/StartPattern.java
@@ -0,0 +1,23 @@
+package basic;
+
+import java.util.Scanner;
+
+public class StartPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        staircase(n);
+    }
+
+    private static void staircase(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
